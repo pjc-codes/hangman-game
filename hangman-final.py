56a624f9750afdcbbc80e6f73d9dce0a2e13481f
prathik <prathik.codes@gmail.com>
2024-11-24 19:47:27 -0500

    Update hangman-final.py
    some minor updates

--- hangman-final.py
+++ hangman-final.py
@@ -4,6 +4,7 @@ from time import sleep
 
 # functions for printing different stages of hangman
 
+
 def hang_1():  # head
     print("____", " | ", " O ", sep="\n")  # head
 
@@ -34,26 +35,26 @@ hangman_prints = [hang_1, hang_2, hang_3, hang_4, hang_5, hang_6]
 
 def word_chooser(word_length):  # function for randomly choosing a word
     words = []
-    with open('02 - beginner python 2/hangman/words.txt', 'r') as f:
+    with open('words.txt', 'r') as f:
         line = f.readline().strip()
         words.append(line)
         while line:
-            line = f.readline().strip()
+            line: str = f.readline().strip()
             words.append(line)
 
     while True:
-        random_index = random.randint(0, len(words))
+        random_index: int = random.randint(0, len(words)-1)
         if (len(words[random_index])) == word_length:
             game_word = words[random_index].upper()
             break
-    # game_word = "BUSY" #for testing
+
     return game_word
 
 
-def get_guess():
+def get_guess() -> str:
     while True:
         try:
-            guess = input(f"\n >>> What is your guess? : ").upper()
+            guess: str = input(f"\n >>> What is your guess? : ").upper()
             if len(guess) == 1 and guess.isalpha():
                 return guess
             else:
@@ -70,12 +71,11 @@ def game(game_word, word_length):  # function for actually running the game
     guessed_word = [" _ " for i in range(word_length)]
     print(f"\n {"".join(guessed_word)}", end="\n")
 
-    guessed_letters = set()  # creating an empty set
+    guessed_letters = set()  # creating an empty set to keep track of guessed letters
     counter = 1
 
     while guessed_word.count(" _ ") and counter <= 6:
-        guess = get_guess()
-        # guess = input(f"\n >>> What is your guess? : ").upper()
+        guess: str = get_guess()
         print()
         if guess in guessed_letters:
             print(f"Already attempted this letter {guess}", end="\n")
